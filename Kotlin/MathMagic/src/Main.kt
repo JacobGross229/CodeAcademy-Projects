41355ccd4137cc5075835f5e383e457544290320
Jake <jacob36926@gmail.com>
2026-08-21 20:58:12 -0400

    Initial pushes

---
+++ Kotlin/MathMagic/src/Main.kt
@@ -0,0 +1,12 @@
+fun main () {
+    val userName = "Jake"
+    var magicNum: Int
+
+    magicNum = 2 * 5
+    magicNum += 3
+    magicNum *= 2
+    magicNum + 6
+    magicNum - 6
+
+    println("$userName's age is $magicNum.")
+}
