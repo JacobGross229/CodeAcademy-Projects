3a4b8259829e6d9eb118ddbac8345550d8f5baf1
Jake <jacob36926@gmail.com>
2026-08-21 21:07:06 -0400

    Initial pushes

---
+++ JavaScript/Dog-Years/Dog-Years.js
@@ -0,0 +1,7 @@
+const myAge = 25 - 2;
+let earlyYears = 2 * 10.5;
+let laterYears = myAge * 4;
+const myAgeInDogYears = earlyYears + laterYears;
+const myName = "Jake".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`)
---
+++ JavaScript/Kelvin-Weather/Kelvin-Weather.js
@@ -0,0 +1,10 @@
+//Declares Kelvin is equal to 293
+const kelvin = 293;
+
+//subtracts 273 from Kelvin.
+let celsius = kelvin - 273;
+
+//Calculates Fahrenheit
+const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
