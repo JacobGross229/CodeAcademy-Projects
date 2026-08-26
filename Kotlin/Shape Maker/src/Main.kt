11818d4bb29d6f5acf8e770ddeee8920d20a7aaa
Jake <jacob36926@gmail.com>
2026-08-26 19:53:53 -0400

    Shape Maker project added

---
+++ Kotlin/Shape Maker/src/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // Square Variables
+    val sqSide = 7
+    val sqChar1 = "X  "
+    val sqChar2 = "O  "
+    // Write your code below
+
+    // Triangle Variables
+    val triRows = 10
+    var triCount = 0
+    var triRowLen = triRows
+    val triChar1 = "/  "
+    val triChar2 = "   "
+    // Write your code below
+
+
+}
