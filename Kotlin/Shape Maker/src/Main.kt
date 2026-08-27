624d5f4a9074e452d397898f371d97b072f7a47d
Jake <jacob36926@gmail.com>
2026-08-26 20:13:45 -0400

    Shape Maker project

--- Kotlin/Shape Maker/src/Main.kt
+++ Kotlin/Shape Maker/src/Main.kt
@@ -4,6 +4,16 @@ fun main() {
     val sqChar1 = "X  "
     val sqChar2 = "O  "
     // Write your code below
+    for (row in 1..sqSide) {
+        for (col in 1..sqSide) {
+            if (row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1) {
+                println(sqChar1)
+            } else {
+                print(sqChar2)
+            }
+        }
+        println()
+    }
 
     // Triangle Variables
     val triRows = 10
@@ -12,6 +22,12 @@ fun main() {
     val triChar1 = "/  "
     val triChar2 = "   "
     // Write your code below
-
+    for (triRow in triRows downTo 1) {
+        while (triCount < triRowLen) {
+            triCount++
+            print(triChar1)
+        }
+        println()
+    }
 
 }
