bf14b762496705263b99298721a0d068c1152a26
Jake <jacob36926@gmail.com>
2026-08-28 18:30:14 -0400

    DIY Functions

--- Kotlin/DIY Functions/src/Main.kt
+++ Kotlin/DIY Functions/src/Main.kt
@@ -1,10 +1,21 @@
 // Write your code below
+fun getListOfNumbers(): Int {
+    var myList = mutableListOf<>()
+        for (i in 1..7) {
+            println("Please enter a number: ")
+            val answer = Integer.valueOf(readLine())
+            myList.add()
+        }
+        return myList
+}
 
-
+fun findMax(): Int {
+ var
+}
 
 fun main() {
     // Write more code below
-
-
+    var values = getListOfNumbers()
+    println(values)
 }
 
