86334e8128d47e33d3ced01bba8eb98b8e90f477
Jake <jacob36926@gmail.com>
2026-08-28 18:55:11 -0400

    DIY Functions Setup

--- Kotlin/DIY Functions/src/Main.kt
+++ Kotlin/DIY Functions/src/Main.kt
@@ -5,17 +5,51 @@ fun getListOfNumbers(): Int {
             println("Please enter a number: ")
             val answer = Integer.valueOf(readLine())
             myList.add()
+            return myList
         }
-        return myList
 }
 
 fun findMax(): Int {
- var
+    var largestNumber = myList[0]
+
+    for (largestElement in myList) {
+        if (largestElement > largestNumber) {
+            largestNumber = largestElement
+        }
+    }
+    return largestNumber
+}
+
+fun findmin(): Int {
+    var smallestNumber = myList[0]
+
+    for (smallestElement in myList) {
+        if (smallestElement < smallestNumber) {
+            smallestNumber = smallestElement
+            return smallestNumber
+        }
+    }
+}
+
+fun findAverage(): Int {
+    var sum = 0
+
+    for (averageElement in myList) {
+        averageElement + sum
+    }
+    return sum / averageElement
 }
 
 fun main() {
     // Write more code below
     var values = getListOfNumbers()
+    var largestValue = findmax(values)
+    var smallestValue = findmin(values)
+    var average = findAverage(values)
+
     println(values)
+    println("The largest number is $largestValue")
+    println("The smallest number is $smallestValue")
+    println("The average is $average")
 }
 
